3d7fc840e45a8b1b71f559b4ec6bc994657f3134
Lekavicius <lekovic.marko@gmail.com>
2023-01-31 19:45:33 +0100

    Ubacujem drugu verziju Hotela.

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 let hamburgerMenu = document.querySelector(".icon");
-
-hamburgerMenu.addEventListener("click", showDisplay);
-
-function showDisplay() {
-    let links = document.querySelector("#menu");
-
-    if (links.style.display === "none") {
-        links.style.display = "block";
-    }
-    else {
-        links.style.display = "none";
-    }
-};
-
-
 const inputName = document.querySelector("#name");
 const inputEmail = document.querySelector("#email");
 const inputMessage = document.querySelector("#message")
@@ -21,20 +6,39 @@ const nameError = document.querySelector("#name-error");
 const emailError = document.querySelector("#email-error");
 const form = document.querySelector("#forma");
 
-let users = [];
+hamburgerMenu.addEventListener("click", showDisplay);
+
 let isValid = false;
 
-form.addEventListener("submit", (event)=>{
+function showDisplay() {
+    let links = document.querySelector("#menu");
+    console.log(links.style.display);
+    if (!links.style.display || links.style.display === "none") {
+        links.style.display = "block";
+        console.log("here");
+    }
+    else {
+        links.style.display = "none";
+    }
+};
+
+
+
+
+form && form.addEventListener("submit", (event)=>{
     event.preventDefault();
    
 
+    nameError.textContent = "";
     if (inputName.value == "") {
         nameError.textContent = "This field is required";
     }
-   else if (inputName.value.length < 5) {
-    nameError.textContent = "This field must have 5 characters.";
-   }
+    else if (inputName.value.length < 5) {
+        nameError.textContent = "This field must have 5 characters.";
+    }
+   
 
+    emailError.textContent = "";
     if (inputEmail.value == "") {
         emailError.textContent = "This field is required"
     }
@@ -42,8 +46,11 @@ form.addEventListener("submit", (event)=>{
         emailError.textContent = "Entered email adress is not valid."
     }
 
-    
-    inputName.value = "";
-    inputEmail.value = "";
-    inputMessage.value = "";
+
+    if (nameError.textContent || emailError.textContent) {
+        console.log("Proslo");
+    }else {
+        console.log("Nije");
+    }
+
 })
